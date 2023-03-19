83a7ce8c07cf9f58e76a944ec046472724cba88b
ruabsarnul <122334016+ruabsarnul@users.noreply.github.com>
2023-03-19 20:28:35 +0600

    updated

--- variable.asm
+++ variable.asm
@@ -1,3 +1,26 @@
+.model small
+.stack 100h
+.data
+
+variablename db 9   ; variable value = 10
+.code 
+
+main proc
+    mov ax, @data 
+    mov ds, ax
+    
+    mov ah,2
+    add variablename, 48
+    mov dl,variablename
+    int 21h    
+    
+    
+    exit:
+    mov ah, 4ch
+    int 21h
+    endp
+ end main
+
 
 
 
