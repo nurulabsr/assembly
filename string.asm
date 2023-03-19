c29d5bdfd2597ed6cccbfb5f1acb9094b4dcca36
ruabsarnul <122334016+ruabsarnul@users.noreply.github.com>
2023-03-19 22:56:36 +0600

    updated

---
+++ string.asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+.data
+
+message db "Learning is one of the best way to update human groth.........$"
+
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    
+    mov ah, 9
+    lea dx, message
+    int 21h
+    
+    
+    exit:
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
+    
+    
+    
+    
+
+
+
+
