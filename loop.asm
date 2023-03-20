dde1c240e37c1c4f6dbd7d6eb0081fc3a5b26711
ruabsarnul <122334016+ruabsarnul@users.noreply.github.com>
2023-03-20 13:29:59 +0600

    updated

--- loop.asm
+++ loop.asm
@@ -13,11 +13,29 @@ main proc
     lea dx,message
     int 21h
     
-   mov ah,2
-   mov dl,10
-   int 21h
-   mov dl,13
-   int 21h  
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h  
+    
+    mov cx, 40
+    mov ah, 2
+    mov dl, "A"
+    
+    
+    level1:
+    int 21h
+    inc dl
+    loop level1
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
+    
+   
    
    
    
