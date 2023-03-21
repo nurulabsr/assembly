3845f3ddbd25137d07ca6db1de72bb8c2c891075
ruabsarnul <122334016+ruabsarnul@users.noreply.github.com>
2023-03-21 16:00:50 +0600

    updated line break and fun 9

--- add_four_num.asm
+++ add_four_num.asm
@@ -3,34 +3,58 @@
 .data
 
 var_1 db ?
-var_2 db ?
+var_2 db ? 
+message_1 db "Enter your frist number: $"
+message_2 db 10,13, "Enter your second number: $"
+message_3 db 10,13, "Entet your third number: $"
+message_4 db 10,13, "Enter your fourh number: $"
+result    db 10,13, "Result: $"
 .code 
 
 main proc 
     mov ax, @data
     mov ds, ax
     
-    
+    mov ah,9
+    lea dx, message_1
+    int 21h
+     
     
     mov ah, 1
     int 21h
     mov ch, al
+   
+   
+    mov ah, 9
+    lea dx, message_2
+    int 21h
     
     
     mov ah, 1
     int 21h
     mov cl, al
     
+    mov ah, 9
+    lea dx, message_3
+    int 21h
     
     mov ah,1
     int 21h
     mov bl, al
+    
+    mov ah, 9
+    lea dx, message_4
+    int 21h
     
     
     mov ah, 1
     int 21h
     mov bh, al
     
+    
+    mov ah, 9
+    lea dx, result
+    int 21h
     
     add ch, cl
     sub ch,48
